0c7b43d1992935969d7dcb222c1ef0032098d6d5
Nguyen Minh Nhat <minhtientiny@gmail.com>
2024-11-30 02:04:03 +0700

    Update ESP.lua

--- ScriptStorage/GeneralScripts/ESP.lua
+++ ScriptStorage/GeneralScripts/ESP.lua
@@ -1,5 +1,4 @@
 -- Simple ESP displaying each player's body parts.
--- https://github.com/MainDabRblx/ProjectDab/blob/master/roblox.png?raw=true
 local color = BrickColor.new(50,0,250)
 local transparency = .8
 
