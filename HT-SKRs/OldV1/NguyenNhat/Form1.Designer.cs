30712ba3badd64018173c284d0058beabc54ea91
Nguyen Minh Nhat <minhtientiny@gmail.com>
2024-12-05 03:26:57 +0700

    add file via upload

---
+++ HT-SKRs/OldV1/NguyenNhat/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace HTSKR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.Button button2;
+            this.execute = new System.Windows.Forms.Button();
+            this.Inject = new System.Windows.Forms.Button();
+            this.Editor = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openfile = new System.Windows.Forms.Button();
+            this.savefile = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Editor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // execute
+            // 
+            this.execute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.execute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.execute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.execute.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.execute.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.execute.Image = ((System.Drawing.Image)(resources.GetObject("execute.Image")));
+            this.execute.Location = new System.Drawing.Point(8, 313);
+            this.execute.Margin = new System.Windows.Forms.Padding(2);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(41, 38);
+            this.execute.TabIndex = 0;
+            this.execute.UseVisualStyleBackColor = false;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // Inject
+            // 
+            this.Inject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Inject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.Inject.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Inject.ForeColor = System.Drawing.SystemColors.Control;
+            this.Inject.Image = ((System.Drawing.Image)(resources.GetObject("Inject.Image")));
+            this.Inject.Location = new System.Drawing.Point(611, 313);
+            this.Inject.Margin = new System.Windows.Forms.Padding(2);
+            this.Inject.Name = "Inject";
+            this.Inject.Size = new System.Drawing.Size(50, 38);
+            this.Inject.TabIndex = 1;
+            this.Inject.UseVisualStyleBackColor = false;
+            this.Inject.Click += new System.EventHandler(this.Inject_Click);
+            // 
+            // Editor
+            // 
+            this.Editor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Editor.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.Editor.AutoScrollMinSize = new System.Drawing.Size(507, 14);
+            this.Editor.BackBrush = null;
+            this.Editor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.Editor.CharHeight = 14;
+            this.Editor.CharWidth = 8;
+            this.Editor.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Editor.DisabledColor = System.Drawing.Color.Gray;
+            this.Editor.ForeColor = System.Drawing.SystemColors.Control;
+            this.Editor.IsReplaceMode = false;
+            this.Editor.Location = new System.Drawing.Point(8, 36);
+            this.Editor.Margin = new System.Windows.Forms.Padding(2);
+            this.Editor.Name = "Editor";
+            this.Editor.Paddings = new System.Windows.Forms.Padding(0);
+            this.Editor.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.Editor.ServiceColors = null;
+            this.Editor.ServiceLinesColor = System.Drawing.Color.LightGray;
+            this.Editor.Size = new System.Drawing.Size(650, 269);
+            this.Editor.TabIndex = 2;
+            this.Editor.Text = "-- By Nguyen Minh Nhat | Join Discord: discord.gg/Hk4FTJt9sf";
+            this.Editor.Zoom = 100;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Pricedown Bl", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(35, -2);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(675, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "HTSKR EXECUTOR";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.BackColor = System.Drawing.Color.Transparent;
+            this.status.ForeColor = System.Drawing.Color.White;
+            this.status.Location = new System.Drawing.Point(156, 12);
+            this.status.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(18, 14);
+            this.status.TabIndex = 5;
+            this.status.Text = "⚪";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(53, 313);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 38);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            button2.BackColor = System.Drawing.Color.Black;
+            button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            button2.Location = new System.Drawing.Point(11, 6);
+            button2.Margin = new System.Windows.Forms.Padding(2);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(24, 26);
+            button2.TabIndex = 8;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(99, 313);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(42, 38);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(638, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(23, 24);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(611, 4);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(23, 24);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openfile
+            // 
+            this.openfile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.openfile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.openfile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.openfile.ForeColor = System.Drawing.SystemColors.Control;
+            this.openfile.Image = ((System.Drawing.Image)(resources.GetObject("openfile.Image")));
+            this.openfile.Location = new System.Drawing.Point(527, 314);
+            this.openfile.Margin = new System.Windows.Forms.Padding(2);
+            this.openfile.Name = "openfile";
+            this.openfile.Size = new System.Drawing.Size(38, 38);
+            this.openfile.TabIndex = 14;
+            this.openfile.UseVisualStyleBackColor = false;
+            this.openfile.Click += new System.EventHandler(this.openfile_Click);
+            // 
+            // savefile
+            // 
+            this.savefile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.savefile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.savefile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.savefile.ForeColor = System.Drawing.SystemColors.Control;
+            this.savefile.Image = ((System.Drawing.Image)(resources.GetObject("savefile.Image")));
+            this.savefile.Location = new System.Drawing.Point(569, 313);
+            this.savefile.Margin = new System.Windows.Forms.Padding(2);
+            this.savefile.Name = "savefile";
+            this.savefile.Size = new System.Drawing.Size(38, 38);
+            this.savefile.TabIndex = 15;
+            this.savefile.UseVisualStyleBackColor = false;
+            this.savefile.Click += new System.EventHandler(this.savefile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(669, 359);
+            this.Controls.Add(this.savefile);
+            this.Controls.Add(this.openfile);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Editor);
+            this.Controls.Add(this.Inject);
+            this.Controls.Add(this.execute);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Inject";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.Editor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Button Inject;
+        private FastColoredTextBoxNS.FastColoredTextBox Editor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button openfile;
+        private System.Windows.Forms.Button savefile;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ HT-SKRs/OldV1/NguyenNhat/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ForlornApi;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace HTSKR
+{
+    public partial class Form1 : RoundedForm
+    {
+        Timer time = new Timer();
+        public Point mouseLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+            time.Tick += timertick;
+            time.Start();
+        }
+
+        private void timertick(object sender, EventArgs e)
+        {
+            if (ForlornApi.Api.IsInjected())
+            {
+                status.Text = "🟢";
+                status.ForeColor = Color.LightGreen;  // Change text color to green
+            }
+            else
+            {
+                status.Text = "🔴";
+                status.ForeColor = Color.Red;  // Change text color to red
+            }
+        }
+
+
+        private void Inject_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.Inject();
+        }
+
+        private void execute_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.ExecuteScript(Editor.Text);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            ForlornApi.Api.SetAutoInject(true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn có muốn tắt Roblox không?", "Thông Báo !!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                ForlornApi.Api.KillRoblox();
+            }
+        }
+
+        private void robloxopen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Editor.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        class functions
+        {
+            public static void PopulateListBox(System.Windows.Forms.ListBox lsb, string Folder, string FileType)
+            {
+                DirectoryInfo dinfo = new DirectoryInfo(Folder);
+                FileInfo[] Files = dinfo.GetFiles(FileType);
+                foreach (FileInfo file in Files)
+                {
+                    lsb.Items.Add(file.Name);
+                }
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePose = Control.MousePosition;
+                mousePose.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePose;
+            }
+        }
+
+        private void openfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Filter = "Tệp Lua (*.lua)|*.lua|Tệp văn bản (*.txt)|*.txt",
+                Title = "Mở Lua hoặc tệp văn bản"
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Editor.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void savefile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Filter = "Tệp Lua (*.lua)|*.lua|Tệp văn bản (*.txt)|*.txt",
+                DefaultExt = "lua",
+                Title = "Lưu Lua hoặc tệp văn bản"
+            };
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string textToSave = Editor.Text;
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.Create))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(textToSave);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Tham gia kênh Discord của chúng tôi, Để nhận thông báo update mới nhất?", "Thông Báo !!", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            if (dialogResult == DialogResult.OK)
+            {
+                System.Diagnostics.Process.Start("https://discord.com/Hk4FTJt9sf");
+            }
+        }
+    }
+}
